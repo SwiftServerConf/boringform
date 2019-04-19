3b0822ca54118cbc69940bf4e5e9fc96b97c40be
Martin Lasek <martinlasek@hotmail.de>
2019-04-19 09:22:35 +0200

    Reading database information from environment variable (with default values)

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,11 +15,11 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   try services.register(FluentMySQLProvider())
   
   let mysqlConfig = MySQLDatabaseConfig(
-    hostname: "127.0.0.1",
+    hostname: Environment.get("DATABASE_HOSTNAME") ?? "127.0.0.1",
     port: 3306,
-    username: "vapor_username",
-    password: "vapor_password",
-    database: "vapor_database"
+    username: Environment.get("DATABASE_USER") ?? "vapor_username",
+    password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+    database: Environment.get("DATABASE_DB") ?? "vapor_database"
   )
   let mysql = MySQLDatabase(config: mysqlConfig)
 
